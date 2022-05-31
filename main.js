12c679cc9bef316d6705f9d4abfa3203024d9dd2
Danielrf033 <99848455+Danielrf033@users.noreply.github.com>
2022-05-31 18:40:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log ("Mi primer proyecto de consola");
+var nombre="Daniel ";
+var apellido="Fernández";
+var nombre_completo= nombre.concat(apellido);
+console.log(nombre_completo);
+var a=10;
+var b=5;
+
+var suma=a+b;
+console.log(suma);
+
+var resta=a-b;
+console.log(resta);
+ 
+var multiplicacion=a*b;
+console.log(multiplicacion);
